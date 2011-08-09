8b901a099466d41bc6d3e2e724504dee9b314e4d
Marcus Kauth <mkauth.mmt-b2009@fh-salzburg.ac.at>
2011-08-09 13:19:40 +0200

    push folder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CompassTest01::Application.config.secret_token = '42b76ef605afc21c2048f727c0abb9865c8d006a4fe48f79a3ae993898b33b23b5656fd6a9003e3eafff182f3b43375ea0944f0a87fe2d6911edf2f79945be14'
